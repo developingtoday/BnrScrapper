ad6c6cc6554ed6176a2a16bae10ec332470f73b7
Stelian Popa <stelian.popa@cegeka.com>
2018-06-21 10:03:49 +0300

    changes of cron job.

--- BnrScrapper/Program.cs
+++ BnrScrapper/Program.cs
@@ -15,7 +15,7 @@ namespace BnrScrapper
         static void Main(string[] args)
         {
             var bnrScrapper = new BnrRateService();
-          //  var robor = bnrScrapper.GetRates(DateTime.Today.AddYears(-25), DateTime.Today).Result;
+            var robor = bnrScrapper.GetRates(DateTime.Today.AddYears(-25), DateTime.Today).Result;
             var euro = bnrScrapper.GetEuroRate(DateTime.Today.AddYears(-25), DateTime.Today).Result;
             var repo = new RateRepository(Environment.GetEnvironmentVariable("DatabaseConnectionString"));
             //repo.InsertBatch(robor);
--- ScrapTimerFunction/ScrapFunction.cs
+++ ScrapTimerFunction/ScrapFunction.cs
@@ -8,14 +8,22 @@ namespace ScrapTimerFunction
     public static class ScrapFunction
     {
         [FunctionName("BnrRoborFunction")]
-        public static void Run([TimerTrigger("0 0 5-12 * * 1-5")]TimerInfo myTimer, TraceWriter log)
+        public static void Run([TimerTrigger("0 0/6 8-8  * * 1-5")]TimerInfo myTimer, TraceWriter log)
         {
-            log.Info("Starting execution");
-            var connString = Environment.GetEnvironmentVariable("DatabaseConnectionString");
-            var manager = new RoborManager(connString,new AzureLogger(log));
-             
-             manager.DoMagic(DateTime.Today, DateTime.Today).Wait();
-            log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
+            try
+            {
+                log.Info("Starting execution");
+                var connString = Environment.GetEnvironmentVariable("DatabaseConnectionString");
+                var manager = new RoborManager(connString, new AzureLogger(log));
+
+                manager.DoMagic(DateTime.Today, DateTime.Today).Wait();
+                log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
+            }
+            catch (Exception ex)
+            {
+                log.Error("An error occurred",ex);
+                throw;
+            }
         }
     }
 }
