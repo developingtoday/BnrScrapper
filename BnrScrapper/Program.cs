1d2cbda70c3b98aaeff368d509a4d7db178d1e31
Stelian Popa <developingtoday@gmail.com>
2018-07-07 23:44:41 +0300

    refactoring finish.

--- BnrScrapper/Program.cs
+++ BnrScrapper/Program.cs
@@ -14,59 +14,19 @@ namespace BnrScrapper
     {
         static void Main(string[] args)
         {
-            double imprumut = 254006.51;
-            int luni = 226;
-            double dobanda = 6.22;
-            var ratalunara = Math.Round(PMT(dobanda, luni, imprumut),2);
-            var lst = new List<object>();
-            var date = new DateTime(2018, 5, 6);
-            var valoareAnticipata = 20000;
-            for (int i=1; i<=luni; i++)
-            {
-                if (i <= 0)
-                {
-                    //after a year simulate what happens anticipate payment;
-                    imprumut -= valoareAnticipata;
-                    Console.WriteLine("Rambursare anticipata");
-                    ratalunara = Math.Round(PMT(dobanda, luni, imprumut),2);
+            var loan = new Loan(254006.51, new DateTime(2018, 5, 6), 6.22, 226);
+            var lst = LoanReturnService.GenerateLoanTransactions(loan);
 
-                }
-                var dobandaVal = Math.Round(imprumut * dobanda / 100 / 12,2);
-                var capital = ratalunara - dobandaVal;
-                imprumut -= capital;
-                var obj = new
-                {
-                    Data=date.AddMonths(i).ToShortDateString(),
-                    dobanda= dobandaVal,
-                    capital,
-                    imprumut=Math.Round(imprumut,2),
-                    ratalunara
-                };
-                if (imprumut <= 0)
-                {
-                    break;
-                }
-                Console.WriteLine(obj);
-                
-                lst.Add(obj);
-                
-            }
-            Console.WriteLine($"Years:{lst.Count / 12}");
-           
+            lst.ForEach(Console.WriteLine);
             Console.WriteLine("E gata smecheria");
             Console.ReadKey();
         }
-        public static double PMT(double yearlyInterestRate, int totalNumberOfMonths, double loanAmount)
-        {
-            var rate = yearlyInterestRate / 100 / 12;
-            var denominator = Math.Pow((1 + rate), totalNumberOfMonths) - 1;
-            return (rate + (rate / denominator)) * loanAmount;
-        }
+ 
     }
 
     public class Loan
     {
-        public Loan(decimal ammount, DateTime rateDateOfPayment, double rate, int months)
+        public Loan(double ammount, DateTime rateDateOfPayment, double rate, int months)
         {
             Ammount = ammount;
             RateDateOfPayment = rateDateOfPayment;
@@ -74,7 +34,7 @@ namespace BnrScrapper
             Months = months;
         }
 
-        public decimal Ammount { get; private set; }
+        public double Ammount { get; private set; }
 
         public DateTime RateDateOfPayment { get; set; }
 
@@ -82,10 +42,33 @@ namespace BnrScrapper
 
         public int Months { get; private set; }
 
+        private static double PMT(double yearlyInterestRate, int totalNumberOfMonths, double loanAmount)
+        {
+            var rate = yearlyInterestRate / 100 / 12;
+            var denominator = Math.Pow((1 + rate), totalNumberOfMonths) - 1;
+            return (rate + (rate / denominator)) * loanAmount;
+        }
+
+        public double MonthlyPayment()
+        {
+            return Math.Round(PMT(Rate, Months, Ammount), 2);
+        }
+
+       
+
     }
 
     public class LoanTransaction
     {
+        public LoanTransaction(DateTime date, double interestValue, double capitalValue, double monthlyPayment, double loanValue)
+        {
+            Date = date;
+            InterestValue = interestValue;
+            CapitalValue = capitalValue;
+            MonthlyPayment = monthlyPayment;
+            LoanValue = loanValue;
+        }
+
         public DateTime Date { get; private set; }
 
         public double InterestValue { get; private set; }
@@ -95,10 +78,37 @@ namespace BnrScrapper
         public double MonthlyPayment { get; private set; }
 
         public double LoanValue { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(Date)}:{Date}, {nameof(InterestValue)}:{InterestValue}, {nameof(CapitalValue)}:{CapitalValue}, {nameof(MonthlyPayment)}:{MonthlyPayment}, {nameof(LoanValue)}:{LoanValue}";
+        }
     }
 
     public class LoanReturnService
     {
+        public static List<LoanTransaction> GenerateLoanTransactions(Loan loan)
+        {
+            var monthlyPayment=loan.MonthlyPayment() ;
+            var loanAmmount = loan.Ammount;
+            var lst = new List<LoanTransaction>();
+            for (int i = 1; i <= loan.Months; i++)
+            {
+                
+                var dobandaVal = Math.Round(loan.Ammount * loan.Rate / 100 / 12, 2);
+                var capital = monthlyPayment - dobandaVal;
+                loanAmmount -= capital;
+                var obj = new LoanTransaction(loan.RateDateOfPayment.AddMonths(i), dobandaVal, capital, monthlyPayment, Math.Round(loanAmmount,2));
+                if (loanAmmount <= 0)
+                {
+                    break;
+                }
+                lst.Add(obj);
 
+            }
+            return lst;
+        }
+
+       
     }
 }
