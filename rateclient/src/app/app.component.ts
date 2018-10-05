cdafc072d04acc473722a3575c651aff47d9deaf
Stelian Popa <stelian.popa@cegeka.com>
2018-10-05 19:31:31 +0300

    first part of application client security.

--- rateclient/src/app/app.component.ts
+++ rateclient/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { AuthService } from './shared/Auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
   title = 'app works!';
+  constructor(private auth:AuthService){
+    // auth.handleAuthentication();
+  }
+
+  public test(){
+    this.auth.login();
+  }
 }
