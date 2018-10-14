6126959951d5ae4bda6a77b3fee329f21ff13c57
Stelian Popa <developingtoday@gmail.com>
2018-10-14 22:59:33 +0300

    add second datepicke and styling.

--- rateclient/src/app/loaneditor/loaneditor.component.ts
+++ rateclient/src/app/loaneditor/loaneditor.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Subscription } from "rxjs";
-import {FormGroup, FormBuilder} from "@angular/forms";
+import { FormGroup, FormBuilder } from "@angular/forms";
 
 @Component({
   selector: "app-loaneditor",
   templateUrl: "./loaneditor.component.html",
   styleUrls: ["./loaneditor.component.scss"]
 })
-export class LoanEditorComponent implements OnInit,OnDestroy {
+export class LoanEditorComponent implements OnInit, OnDestroy {
   private id: number;
   options: FormGroup;
   private sub: Subscription;
-  constructor(fb: FormBuilder,private route:ActivatedRoute) {
+  constructor(fb: FormBuilder, private route: ActivatedRoute) {
     this.options = fb.group({
       hideRequired: false,
-      floatLabel: 'auto',
+      floatLabel: "auto"
     });
   }
   ngOnInit() {
@@ -23,7 +23,7 @@ export class LoanEditorComponent implements OnInit,OnDestroy {
       this.id = +a["id"];
     });
   }
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.sub.unsubscribe();
   }
 }
